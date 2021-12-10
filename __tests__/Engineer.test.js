5977578abc68b877ed2667a0fcdbcb7061b5d219
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-12-10 14:51:23 -0500

    engineer class and tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,40 @@
+const Engineer = require('../lib/Engineer.js')
+
+test('creates engineer object', () => {
+    const engineer = new Engineer('Stitch', '626', 'experiment@626.com', 'github')
+
+    expect(engineer.name).toBe('Stitch')
+    expect(engineer.id).toBe('626')
+    expect(engineer.email).toBe('experiment@626.com')
+    expect(engineer.github).toBe('github')
+})
+
+test('gets engineer name', () => {
+    const engineer = new Engineer('Stitch', '626', 'experiment@626.com')
+
+    expect(engineer.getName()).toEqual(expect.stringContaining(engineer.name.toString()))
+})
+
+test('gets engineer id', () => {
+    const engineer = new Engineer('Stitch', '626', 'experiment@626.com')
+
+    expect(engineer.getId()).toEqual(expect.stringContaining(engineer.id.toString()))
+})
+
+test('gets engineer email', () => {
+    const engineer = new Engineer('Stitch', '626', 'experiment@626.com')
+
+    expect(engineer.getEmail()).toEqual(expect.stringContaining(engineer.email.toString()))
+})
+
+test('gets engineer role', () => {
+    const engineer = new Engineer('Stitch', '626', 'experiment@626.com')
+
+    expect(engineer.getRole()).toEqual(expect.stringContaining(`Engineer`))
+})
+
+test('gets engineer github', () => {
+    const engineer = new Engineer('Stitch', '626', 'experiment@626.com', 'github')    
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()))
+})
