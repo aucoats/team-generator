13f6b6bf0c8c737b4312e6d045925e785604ad58
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-12-10 13:16:29 -0500

    employee tests and class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require('../lib/Employee.js')
+
+test('creates employee object', () => {
+    const employee = new Employee('Stitch', '626', 'experiment@626.com')
+
+    expect(employee.name).toBe('Stitch')
+    expect(employee.id).toBe('626')
+    expect(employee.email).toBe('experiment@626.com')
+})
+
+test('gets employee name', () => {
+    const employee = new Employee('Stitch', '626', 'experiment@626.com')
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()))
+})
+
+test('gets employee id', () => {
+    const employee = new Employee('Stitch', '626', 'experiment@626.com')
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()))
+})
+
+test('gets employee email', () => {
+    const employee = new Employee('Stitch', '626', 'experiment@626.com')
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()))
+})
+
+test('gets employee role', () => {
+    const employee = new Employee('Stitch', '626', 'experiment@626.com')
+
+    expect(employee.getRole()).toEqual(expect.stringContaining(`Employee`))
+})
