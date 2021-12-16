ae6d9ae9dff5919f4ebbe7cb8404e5db0a5a7864
Poke Code <90790144+aucoats@users.noreply.github.com>
2021-12-16 17:39:06 -0500

    added style and font kit to html

--- index.js
+++ index.js
@@ -244,7 +244,28 @@ function completeTeam(team) {
 
 function generateHTML(manager, engineers, interns) {
     return `
-     `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/be05b4af6b.js" crossorigin="anonymous"></script>
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header>
+            <h1>My Team</h1>
+        </header>
+    
+        <main>
+            ${managerHTML(manager)}
+            ${engiHTML(engineers)}
+            ${internHTML(interns)}
+        </main>
+    </body>
+    </html>`
 }
 
 // init()
